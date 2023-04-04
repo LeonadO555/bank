9e35ea76323e14ea1fc2e322e6e51ce630701107
Dushuk Oleksandr <dushuk@gmail.com>
2023-04-04 11:40:08 +0200

    Described method takeAndCompareScreenshot

--- src/test/java/pages/customerLogin/account/deposit/DepositPage.java
+++ src/test/java/pages/customerLogin/account/deposit/DepositPage.java
@@ -1,10 +1,30 @@
 package pages.customerLogin.account.deposit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pages.PageBase;
+import wait.Wait;
 
 public class DepositPage extends PageBase {
     public DepositPage(WebDriver driver) {
         super(driver);
     }
+
+    Wait wait;
+
+
+    @FindBy(xpath = "//input[@placeholder='amount']")
+    protected WebElement amountField;
+
+
+    public void waitForLoading() {
+        wait = new Wait(driver);
+        wait.forVisibility(amountField);
+    }
+
+    public void fillAmountField(String amount) { // нужно переписать, чтоб Integer принимал
+        fillField(amountField, amount);
+    }
+
 }
