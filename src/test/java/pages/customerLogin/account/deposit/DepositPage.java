4cfdfb3cf930139004dd591c5841d9d2de5ab5be
Leonid Mihaylov <leonmixt@gmail.com>
2023-04-04 12:46:32 +0400

    Added pages

---
+++ src/test/java/pages/customerLogin/account/deposit/DepositPage.java
@@ -0,0 +1,10 @@
+package pages.customerLogin.account.deposit;
+
+import org.openqa.selenium.WebDriver;
+import pages.PageBase;
+
+public class DepositPage extends PageBase {
+    public DepositPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/test/java/pages/customerLogin/account/withdrawl/WithdrawPage.java
@@ -0,0 +1,10 @@
+package pages.customerLogin.account.withdrawl;
+
+import org.openqa.selenium.WebDriver;
+import pages.PageBase;
+
+public class WithdrawPage extends PageBase {
+    public WithdrawPage(WebDriver driver) {
+        super(driver);
+    }
+}
