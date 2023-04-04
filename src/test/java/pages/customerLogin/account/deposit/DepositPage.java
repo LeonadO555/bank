b42c8d1f50dd0b1bc7abead4f561fc1b7aaee243
Olchik5 <98747632+Olchik5@users.noreply.github.com>
2023-04-04 11:31:21 +0200

    Added clickOnWithdrawButton method

--- src/test/java/pages/customerLogin/account/deposit/DepositPage.java
+++ src/test/java/pages/customerLogin/account/deposit/DepositPage.java
@@ -1,10 +1,29 @@
 package pages.customerLogin.account.deposit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pages.PageBase;
+import wait.Wait;
 
 public class DepositPage extends PageBase {
+
+    Wait wait;
+
+    public void waitForLoading() {
+        wait = new Wait(driver);
+        wait.forVisibility(withdraw);
+    }
+
     public DepositPage(WebDriver driver) {
         super(driver);
     }
+
+    @FindBy(css = ".btn-default")
+    WebElement withdraw;
+
+    public void clickOnWithdrawButton() {
+        click(withdraw);
+    }
 }
+
