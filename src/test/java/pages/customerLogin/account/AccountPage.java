1b3a41fc43727a0c1d12cd8851f25649902f4d65
Pavel <pavelkelbas@gmail.com>
2023-04-04 11:26:40 +0200

    added AccountPage methode

--- src/test/java/pages/customerLogin/account/AccountPage.java
+++ src/test/java/pages/customerLogin/account/AccountPage.java
@@ -16,8 +16,45 @@ public class AccountPage extends PageBase {
     @FindBy(xpath = "//*[@ng-click='byebye()']")
     protected WebElement logoutButton;
 
+    @FindBy(xpath = "//button[normalize-space()='Transactions']")
+    protected WebElement transactionsButton;
+
+    @FindBy(xpath = "//button[normalize-space()='Deposit']")
+    protected WebElement depositButton;
+
+    @FindBy(xpath = "//button[normalize-space()='Withdrawl']")
+    protected WebElement withdrawButton;
+
+    @FindBy(xpath = "//select[@id='accountSelect']")
+    protected WebElement accountSelect;
+
     public void waitForLoading() {
         wait = new Wait(driver);
         wait.forVisibility(logoutButton);
+        wait.forVisibility(transactionsButton);
+        wait.forVisibility(depositButton);
+        wait.forVisibility(withdrawButton);
+        wait.forVisibility(accountSelect);
+    }
+
+
+    public void clickLogOutButton() {
+        click(logoutButton);
+    }
+
+    public void clickTransactionsButton() {
+        click(transactionsButton);
+    }
+
+    public void clickDepositButton() {
+        click(depositButton);
+    }
+
+    public void clickWithdrawButton() {
+        click(withdrawButton);
+    }
+
+    public void selectCurrencyAccount(String accountId) {
+        selectOption(accountId, accountSelect);
     }
 }
